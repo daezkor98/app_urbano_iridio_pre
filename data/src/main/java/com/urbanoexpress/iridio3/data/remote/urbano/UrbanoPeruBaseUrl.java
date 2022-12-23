2a56104b98399090867ef74b6832c7b91ad38d25
brandon <bquintanilla@urbano.com.pe>
2022-12-23 17:45:12 -0500

    MOD-> changed DEV base URL

--- data/src/main/java/com/urbanoexpress/iridio3/data/remote/urbano/UrbanoPeruBaseUrl.java
+++ data/src/main/java/com/urbanoexpress/iridio3/data/remote/urbano/UrbanoPeruBaseUrl.java
@@ -5,7 +5,7 @@ import com.urbanoexpress.iridio3.data.remote.BaseUrl;
 
 public final class UrbanoPeruBaseUrl implements BaseUrl {
 
-    private static final String DEVELOPMENT_BASE_URL = "https://dev.urbanoexpress.com.pe/pyp/";
+    private static final String DEVELOPMENT_BASE_URL = "https://pyp3.pe.urbanoexpress.net/";
     private static final String PRODUCTION_BASE_URL = "https://app.urbano.com.pe/";
 
     private final int apiEnvironment;
