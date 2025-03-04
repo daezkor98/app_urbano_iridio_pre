dca3dd6fa5e2509996e95dd5b19c5ebb9bfe2f32
Juninho <urbano.express.holding@gmail.com>
2025-03-04 15:56:10 -0500

    plan ruta base flow

---
+++ app/src/main/java/com/urbanoexpress/iridio3/pe/util/constant/PlanRutaConstants.kt
@@ -0,0 +1,24 @@
+package com.urbanoexpress.iridio3.pe.util.constant
+
+
+/**
+ * Created by Brandon Quintanilla on Febrero/25/2025.
+ */
+object PlanRutaConstants {
+    const val ERROR_400 = "400: "
+    const val ERROR_409 = "409: "
+    const val ERROR_MSG = "msg"
+    const val RESPONSE_ERROR = "msg_error"
+    const val GENERIC_ERROR = "ocurrio un error no definido"
+    const val SUCCESS = "success"
+    const val EMPTY_VALUE = ""
+    const val PREFERENCES_PHONE = "phone"
+    const val PREFERENCES_ID_PER = "idPer"
+    const val PREFERENCES_GLOBAL_CONFIG = "GlobalConfigApp"
+    const val PREFERENCES_USER_PROFILE = "UserProfile"
+
+    const val MODULE_NAME = "module_name"
+    const val MODULE_TITLE = "Ruta del Dia"
+
+
+}
